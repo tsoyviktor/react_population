3a615f50bdbe1fc6c15c8e0171e2757bf1bad32e
Valerii Martseniuk <valerii@webinato.com>
2017-07-23 21:16:17 -0700

    atmpt 2

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import ReduxApp from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 // Fix to make it work on Heroku
-const injectTapEventPlugin = require("react-tap-event-plugin");
-injectTapEventPlugin();
+// const injectTapEventPlugin = require("react-tap-event-plugin");
+// injectTapEventPlugin();
 // End of fix
 
 ReactDOM.render(<ReduxApp />, document.getElementById('root'));
