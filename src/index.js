23b7c1af865d244d71ba6a2d4b5211be84ab9067
Valerii Martseniuk <valerii@webinato.com>
2017-07-23 21:06:46 -0700

    tryint to fix tap plugin to make it work on heroku

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReduxApp from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
+// Fix to make it work on Heroku
+const injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
+// End of fix
 
 ReactDOM.render(<ReduxApp />, document.getElementById('root'));
 registerServiceWorker();
